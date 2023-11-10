390d98dd192c3aefca305821822dc90670a6e5bb
Evgeny Shtykhno <denisiksht.k9@gmail.com>
2023-11-10 17:48:23 +0200

    add file

---
+++ main.py
@@ -0,0 +1,23 @@
+result = []
+
+def divider(a, b):
+    try:
+        if a < b:
+            raise ValueError("a should be greater than or equal to b")
+        if b > 100:
+            raise IndexError("b should be less than or equal to 100")
+        return a / b
+    except ValueError as ve:
+        print(f"ValueError: {ve}")
+    except IndexError as ie:
+        print(f"IndexError: {ie}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
+
+for key in data:
+    res = divider(key, data[key])
+    result.append(res)
+
+print(result)
